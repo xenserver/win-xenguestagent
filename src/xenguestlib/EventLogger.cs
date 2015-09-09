620ddceb6b1cbbdb916ad9a49776a3ed2187580c
Cheng Zhang <cheng.zhang@citrix.com>
2015-09-09 11:25:15 +0800

    CP-14007: Add more event logs for guest agent automatic updates
    Signed-off-by: Cheng Zhang <cheng.zhang@citrix.com>

---
+++ src/xenguestlib/EventLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace xenwinsvc
+{
+    public class EventLogger
+    {
+        static EventLog el;
+        static EventLogger()
+        {
+            el = new EventLog();
+            el.Source = "XenGuestAgent";
+            if (!EventLog.SourceExists("XenGuestAgent"))
+            {
+                //Create New Log       
+                EventLog.CreateEventSource("XenGuestAgent", "XenGuestAgentLog");
+            }
+        }
+
+        public static void addEvent(string message)
+        {
+            //Log Information  
+            el.WriteEntry(message, EventLogEntryType.Information);
+        }
+
+        public static void addException(string message)
+        {
+            //Log Exception  
+            el.WriteEntry(message, EventLogEntryType.Error);
+        }
+
+        public static void addWarning(string message)
+        {
+            //Log Warning  
+            el.WriteEntry(message, EventLogEntryType.Warning);
+        }
+    }
+}
