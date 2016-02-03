278d717e75c3cff5a94e89e3505db064b9e83c4c
Owen Smith <owen.smith@citrix.com>
2016-02-03 17:30:51 +0000

    Add Branding class to pull in branded strings
    Signed-off-by: Owen Smith <owen.smith@citrix.com>

---
+++ src/xenupdater/Branding.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using BrandSupport;
+
+
+namespace XenUpdater
+{
+    public class Branding
+    {
+        private static BrandingControl instance;
+
+        public static BrandingControl Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    Assembly assembly = Assembly.GetExecutingAssembly();
+                    string brandsatpath = Path.GetDirectoryName(assembly.Location) + "\\Branding\\brandsat.dll";
+                    instance = new BrandingControl(brandsatpath);
+                }
+
+                return instance;
+            }
+        }
+
+        public static string GetString(string key)
+        {
+            return Instance.getString(key);
+        }
+    }
+}
