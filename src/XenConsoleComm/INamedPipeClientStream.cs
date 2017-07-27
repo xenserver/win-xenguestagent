5703a308dedcb638efe3f9aabe568cf53c215f55
Kostas Ladopoulos <konstantinos.ladopoulos@citrix.com>
2017-07-27 15:28:12 +0000

    Initial commit of the XenConsole communication assemblies
    Adds 'XenConsoleComm' and 'IXenConsoleComm' projects
    
    Signed-off-by: Kostas Ladopoulos <konstantinos.ladopoulos@citrix.com>

---
+++ src/XenConsoleComm/INamedPipeClientStream.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace XenConsoleComm.Interfaces
+{
+    internal interface INamedPipeClientStream
+    {
+        void Connect();
+        IAsyncResult BeginRead(
+            byte[] buffer,
+            int offset,
+            int count,
+            AsyncCallback callback,
+            object state
+        );
+        int EndRead(IAsyncResult asyncResult);
+        void Write(string value);
+
+        bool IsMessageComplete { get; }
+        bool IsConnected { get; }
+    }
+}
---
+++ src/XenConsoleComm/XenConsoleStream.cs
@@ -0,0 +1,100 @@
+﻿using IXenConsoleComm;
+using System;
+using System.IO.Pipes;
+using System.Text;
+using XenConsoleComm.Interfaces;
+using XenConsoleComm.Wrappers;
+
+namespace XenConsoleComm
+{
+    public class XenConsoleStream : IXenConsoleStream
+    {
+        public event EventHandler MessageReceived;
+        public event EventHandler PipeDisconnected;
+
+        private readonly INamedPipeClientStream _xenConsoleClient;
+        private byte[] _readBuffer;
+        private Func<string, bool> _messageForwardingRule = null;
+        private readonly int _readBufferSize = 1024;
+
+        public XenConsoleStream() : this(
+            new NamedPipeClientStreamWrapper(
+                ".",
+                "XenConsoleMonitor",
+                PipeDirection.InOut,
+                PipeOptions.Asynchronous
+            )) { }
+
+        internal XenConsoleStream(INamedPipeClientStream pipeClient)
+        {
+            _xenConsoleClient = pipeClient;
+
+            if (!_xenConsoleClient.IsConnected)
+            {
+                _xenConsoleClient.Connect();
+            }
+
+            _readBuffer = new byte[_readBufferSize];
+
+            _xenConsoleClient.BeginRead(
+                _readBuffer,
+                0,
+                _readBufferSize,
+                new AsyncCallback(OnXenConsoleMessageReceived),
+                null
+            );
+        }
+
+        public Func<string, bool> MessageForwardingRule
+        {
+            get { return _messageForwardingRule; }
+            set { _messageForwardingRule = value; }
+        }
+
+        internal void OnXenConsoleMessageReceived(IAsyncResult ar)
+        {
+            EventHandler msgReceived = MessageReceived;
+            EventHandler pipeDiscon = PipeDisconnected;
+            Func<string, bool> rule = _messageForwardingRule;
+
+            int bytesRead = _xenConsoleClient.EndRead(ar);
+
+            if (!_xenConsoleClient.IsMessageComplete)
+                throw new NotSupportedException(String.Format(
+                    "Message is larger than {0} bytes.",
+                    _readBufferSize
+                ));
+
+            // Console.WriteLine("Message received is {0} bytes.", bytesRead);
+            /*
+            // TODO: Dispose pipe/make object unusable
+            if (bytesRead == 0)
+            {
+                if (pipeDiscon != null)
+                    pipeDiscon(this, EventArgs.Empty);
+
+                return;
+            }
+            */
+            string message = Encoding.UTF8.GetString(_readBuffer, 0, bytesRead).Trim();
+
+            if (msgReceived != null
+                    && (rule == null
+                        || rule(message)))
+            {
+                msgReceived(
+                    this,
+                    new XenConsoleMessageEventArgs(message, _xenConsoleClient)
+                );
+            }
+
+            _xenConsoleClient.BeginRead(
+                _readBuffer,
+                0,
+                _readBufferSize,
+                new AsyncCallback(OnXenConsoleMessageReceived),
+                null
+            );
+        }
+    }
+}
